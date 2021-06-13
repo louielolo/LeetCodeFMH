fc9caae71d35c42c9b34cdbf29725f666c358e6e
louielolo <lu.yanting@qq.com>
2021-06-13 16:00:42 +0800

    update

--- hw/密码验证合格程序/password.py
+++ hw/密码验证合格程序/password.py
@@ -10,21 +10,42 @@
 如果符合要求输出：OK，否则输出NG
 """
 import re
+import sys
 class Solution:
-    def substring_duplicate(self,pw):
-        pass
-    def complex_rule(self,pw):
-        if pw is None or len(pw)<1 or pw.isalpha() or pw.isalnum or pw.isdigit() or pw.isspace():  #是否是纯字母构成,字母和数字的组合,纯数字构成
+    def substring_duplicate(self,pwd):
+        if pwd is not None and len(pwd)>=3:
+            for i in range(len(pwd)-3):
+                if pwd.count(pwd[i:i+3])>1:
+                    return False
+        return True
+    def complex_rule(self,pwd):
+        condition = 0
+        if pwd is None or len(pwd)<1 or pwd.isalpha() or pwd.isdigit() or pwd.isspace():  #是否是纯字母构成,字母和数字的组合,纯数字构成
             return False
-        if re.search(pw)
+        if re.search(r'\d+', pwd):
+            condition = 1
+        if re.search(r'[a-z]+', pwd):
+            condition += 1
+        if re.search(r'[A-Z]+', pwd):
+            condition += 1
+        if re.search(r'[!@#$%^&*()_+=-]+', pwd):
+            condition += 1
+        if condition >=3:
+            return True
+        else:
+            return False
+        return False
 
-    def pw_sw_verify(self,pw):
-        pass
+    def pw_sw_verify(self,pwd):
+        if self.complex_rule(pwd) and self.substring_duplicate(pwd):
+            print('OK')
+        else:
+            print('NG')
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     s = Solution()
     getline = lambda: sys.stdin.readline().strip() #利用lambda定义读取数据函数
-    line = getline()
-    while line:
-        s.pw_sw_verify(line)
-        line = getline()
+    pwd = getline()
+    while pwd:
+        s.pw_sw_verify(pwd)
+        pwd = getline()
